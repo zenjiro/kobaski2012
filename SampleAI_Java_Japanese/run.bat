7a7a7dc8d661ebbb1a580c9631b02fe37a21d893
zenjiro <zenjiro0123@gmail.com>
2012-01-02 16:07:46 +0900

    コピーした方のサンプルAIが動いて4点とった！

--- SampleAI_Java_Japanese/run.bat
+++ SampleAI_Java_Japanese/run.bat
@@ -1,1 +1,1 @@
-java -jar SamuraiCoding2011-2.0.4.jar "java -cp bin jp.ac.waseda.cs.washi.samurai.main.Main"
+java -jar SamuraiCoding2011-2.0.4.jar "java -cp ../zenjiro/target/classes jp.ac.waseda.cs.washi.samurai.main.Main"
