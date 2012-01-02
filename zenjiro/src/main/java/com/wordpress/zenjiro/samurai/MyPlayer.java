3091643eecd90091ed3dba04940127a77ae5ff4f
zenjiro <zenjiro0123@gmail.com>
2012-01-02 16:17:28 +0900

    クリーン

--- zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
+++ zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
@@ -12,13 +12,12 @@ import jp.ac.waseda.cs.washi.samurai.api.Direction;
 import jp.ac.waseda.cs.washi.samurai.api.Map;
 
 public class MyPlayer {
-
 	private int _count = 0;
 	private Map _map;
 
 	// returns a player action
 	private String getRandomAction() {
-		switch (_count * 11 % 5) {
+		switch (this._count * 11 % 5) {
 		case 0:
 			return "NONE";
 		case 1:
@@ -34,64 +33,60 @@ public class MyPlayer {
 		}
 	}
 
-	public void run(Scanner sc) {
+	public void run(final Scanner sc) {
 		// TODO: ここにあなたのAIプログラムを書いてください！
-		_map = Map.createOrUpdateMap(_map, sc);
-
+		this._map = Map.createOrUpdateMap(this._map, sc);
 		// for debug (you can remove this statement)
-		log();
-
-		if (_count % 2 == 0) {
-			System.out.println(getRandomAction());
+		this.log();
+		if (this._count % 2 == 0) {
+			System.out.println(this.getRandomAction());
 		} else {
-			System.out.println(getRandomAction());
+			System.out.println(this.getRandomAction());
 		}
-
-		_count++;
+		this._count++;
 		System.out.flush();
 	}
 
 	// ----------- debug code (you can remove the following code) -----------
-
 	private BufferedWriter _writer;
 
 	public MyPlayer() {
 		try {
-			FileOutputStream fos = new FileOutputStream("log_java_jp.txt");
-			_writer = new BufferedWriter(new OutputStreamWriter(fos));
-		} catch (FileNotFoundException e) {
+			final FileOutputStream fos = new FileOutputStream("log_java_jp.txt");
+			this._writer = new BufferedWriter(new OutputStreamWriter(fos));
+		} catch (final FileNotFoundException e) {
 		}
 	}
 
 	public void log() {
 		try {
-			_writer.write(_map.getRemainingTime() + ", ");
-			_writer.write(_map.getPlayerIndex() + ", ");
-			_writer.write(_map.getWidth() + ", ");
-			_writer.write(_map.getHeight() + "\r\n");
-
-			for (Chara chara : _map.getAllCharas()) {
+			this._writer.write(this._map.getRemainingTime() + ", ");
+			this._writer.write(this._map.getPlayerIndex() + ", ");
+			this._writer.write(this._map.getWidth() + ", ");
+			this._writer.write(this._map.getHeight() + "\r\n");
+			for (final Chara chara : this._map.getAllCharas()) {
 				for (int i = 0; i < 4; i++) {
-					_writer.write(_map.isMovable(chara, Direction.values()[i])
+					this._writer.write(this._map.isMovable(chara,
+							Direction.values()[i])
 							+ ", ");
 				}
-				_writer.write("\r\n");
+				this._writer.write("\r\n");
 			}
-
-			for (Chara samurai : _map.getAllSamurais()) {
-				_writer.write(_map.isAvailable(samurai.getX(), samurai.getY())
-						+ ", ");
-				_writer.write("{ X = " + samurai.getX() + ", Y = "
+			for (final Chara samurai : this._map.getAllSamurais()) {
+				this._writer.write(this._map.isAvailable(samurai.getX(),
+						samurai.getY()) + ", ");
+				this._writer.write("{ X = " + samurai.getX() + ", Y = "
 						+ samurai.getY() + " }" + "\r\n");
 			}
-
-			for (Chara dog : _map.getAllDogs()) {
-				_writer.write(_map.isAvailable(dog.getX(), dog.getY()) + ", ");
-				_writer.write("{ X = " + dog.getX() + ", Y = " + dog.getY()
-						+ " }" + "\r\n");
+			for (final Chara dog : this._map.getAllDogs()) {
+				this._writer
+						.write(this._map.isAvailable(dog.getX(), dog.getY())
+								+ ", ");
+				this._writer.write("{ X = " + dog.getX() + ", Y = "
+						+ dog.getY() + " }" + "\r\n");
 			}
-			_writer.flush();
-		} catch (IOException e) {
+			this._writer.flush();
+		} catch (final IOException e) {
 		}
 	}
 	// ----------------------------------------------------------------------
