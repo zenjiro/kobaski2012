b4ec892fcc287f663115616ebc160c8b5e893170
zenjiro <zenjiro0123@gmail.com>
2012-01-04 20:38:09 +0900

    サンプルを削除

--- SampleAI_Java_Japanese/compile.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ./bin/jp/ac/waseda/cs/washi/samurai/api/*.class
-rm -rf ./bin/jp/ac/waseda/cs/washi/samurai/main/*.class
-mkdir bin
-javac -cp src -d bin src/jp/ac/waseda/cs/washi/samurai/main/*.java -encoding UTF-8
--- SampleAI_Java_Japanese/run.sh
+++
@@ -1,1 +0,0 @@
-java -jar SamuraiCoding2011-2.0.4.jar "java -cp bin jp.ac.waseda.cs.washi.samurai.main.Main"
