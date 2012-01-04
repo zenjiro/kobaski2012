901c8ab5ae623b248b119305cec6a20748d9f9c4
zenjiro <zenjiro0123@gmail.com>
2012-01-04 21:24:31 +0900

    ソート

--- zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
+++ zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
@@ -47,112 +47,6 @@ public class MyPlayer implements Player {
 		}
 	}
 
-	/**
-	 * @param chara 始点とするChara
-	 * @param hateDogs 敵の犬を避けるかどうか
-	 * @param map マップ
-	 * @return 指定したCharaから各地点への最短距離と経路
-	 */
-	private Distance getDistance(final Chara chara, final boolean hateDogs, final Map map) {
-		final int width = map.getWidth();
-		final int height = map.getHeight();
-		final int[][] distance = new int[height][width];
-		final Direction[][] path = new Direction[height][width];
-		final boolean[][] dogs = new boolean[height][width];
-		for (int i = 0; i < height; i++) {
-			distance[i] = new int[width];
-			Arrays.fill(distance[i], Integer.MAX_VALUE);
-			path[i] = new Direction[width];
-			Arrays.fill(path[i], Direction.NONE);
-			dogs[i] = new boolean[width];
-		}
-		if (hateDogs) {
-			for (final Chara dog : map.getAllDogs()) {
-				if (dog != map.getMyDog()) {
-					dogs[dog.getY()][dog.getX()] = true;
-				}
-			}
-		}
-		distance[chara.getY()][chara.getX()] = 0;
-		search(chara.getX(), chara.getY(), distance, path, dogs, map);
-		return new Distance(distance, path);
-	}
-
-	/**
-	 * 深さ優先で探索します。
-	 * @param x 始点のx座標
-	 * @param y 始点のy座標
-	 * @param distance 各地点への最短距離
-	 * @param path 各地点への最短経路
-	 * @param dogs 敵の犬の座標
-	 * @param map マップ
-	 */
-	private void search(final int x, final int y, final int[][] distance, final Direction[][] path,
-			final boolean[][] dogs, final Map map) {
-		final int d = distance[y][x] + 1;
-		for (final Direction direction : Direction.values()) {
-			if (direction != Direction.NONE) {
-				final int dx = direction.dx;
-				final int dy = direction.dy;
-				if (map.isAvailable(x + dx, y + dy) && !map.isWall(x + dx, y + dy)
-						&& !dogs[y + dy][x + dx] && d < distance[y + dy][x + dx]) {
-					distance[y + dy][x + dx] = d;
-					path[y + dy][x + dx] = direction;
-					this.search(x + dx, y + dy, distance, path, dogs, map);
-				}
-			}
-		}
-	}
-
-	/**
-	 * @param x 目的地のx座標
-	 * @param y 目的地のy座標
-	 * @param path 各地点への最短経路
-	 * @return 指定した地点へ向かうパスの最初の方向
-	 */
-	private Direction getDirection(int x, int y, final Direction[][] path) {
-		Direction direction = Direction.NONE;
-		while (path[y][x] != Direction.NONE) {
-			direction = path[y][x];
-			x -= direction.dx;
-			y -= direction.dy;
-		}
-		return direction;
-	}
-
-	/**
-	 * @param x x座標
-	 * @param y y座標
-	 * @param map マップ
-	 * @return 指定した地点に行くと敵の犬に殺されるかどうか
-	 */
-	private boolean isKilled(final int x, final int y, final Map map) {
-		for (final Chara dog : map.getAllDogs()) {
-			if (dog != map.getMyDog()) {
-				if (Math.abs(dog.getX() - x) < 2 && Math.abs(dog.getY() - y) < 2
-						&& (dog.getX() == x || dog.getY() == y)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * @param chara キャラ
-	 * @param map マップ
-	 * @return 安全な向き
-	 */
-	private Direction getSafeDirection(final Chara chara, final Map map) {
-		for (final Direction direction : new Direction[] { Direction.DOWN, Direction.RIGHT,
-				Direction.UP, Direction.LEFT }) {
-			if (!isKilled(chara.getX() + direction.dx, chara.getY() + direction.dy, map)) {
-				return direction;
-			}
-		}
-		return Direction.NONE;
-	}
-
 	@Override
 	public Direction calc(final Map map, final boolean isSamurai) {
 		if (isSamurai) {
@@ -213,6 +107,112 @@ public class MyPlayer implements Player {
 				return this.getDirection(target.getX(), target.getY(), distance.path);
 			} else {
 				return Direction.NONE;
+			}
+		}
+	}
+
+	/**
+	 * @param x 目的地のx座標
+	 * @param y 目的地のy座標
+	 * @param path 各地点への最短経路
+	 * @return 指定した地点へ向かうパスの最初の方向
+	 */
+	private Direction getDirection(int x, int y, final Direction[][] path) {
+		Direction direction = Direction.NONE;
+		while (path[y][x] != Direction.NONE) {
+			direction = path[y][x];
+			x -= direction.dx;
+			y -= direction.dy;
+		}
+		return direction;
+	}
+
+	/**
+	 * @param chara 始点とするChara
+	 * @param hateDogs 敵の犬を避けるかどうか
+	 * @param map マップ
+	 * @return 指定したCharaから各地点への最短距離と経路
+	 */
+	private Distance getDistance(final Chara chara, final boolean hateDogs, final Map map) {
+		final int width = map.getWidth();
+		final int height = map.getHeight();
+		final int[][] distance = new int[height][width];
+		final Direction[][] path = new Direction[height][width];
+		final boolean[][] dogs = new boolean[height][width];
+		for (int i = 0; i < height; i++) {
+			distance[i] = new int[width];
+			Arrays.fill(distance[i], Integer.MAX_VALUE);
+			path[i] = new Direction[width];
+			Arrays.fill(path[i], Direction.NONE);
+			dogs[i] = new boolean[width];
+		}
+		if (hateDogs) {
+			for (final Chara dog : map.getAllDogs()) {
+				if (dog != map.getMyDog()) {
+					dogs[dog.getY()][dog.getX()] = true;
+				}
+			}
+		}
+		distance[chara.getY()][chara.getX()] = 0;
+		search(chara.getX(), chara.getY(), distance, path, dogs, map);
+		return new Distance(distance, path);
+	}
+
+	/**
+	 * @param chara キャラ
+	 * @param map マップ
+	 * @return 安全な向き
+	 */
+	private Direction getSafeDirection(final Chara chara, final Map map) {
+		for (final Direction direction : new Direction[] { Direction.DOWN, Direction.RIGHT,
+				Direction.UP, Direction.LEFT }) {
+			if (!isKilled(chara.getX() + direction.dx, chara.getY() + direction.dy, map)) {
+				return direction;
+			}
+		}
+		return Direction.NONE;
+	}
+
+	/**
+	 * @param x x座標
+	 * @param y y座標
+	 * @param map マップ
+	 * @return 指定した地点に行くと敵の犬に殺されるかどうか
+	 */
+	private boolean isKilled(final int x, final int y, final Map map) {
+		for (final Chara dog : map.getAllDogs()) {
+			if (dog != map.getMyDog()) {
+				if (Math.abs(dog.getX() - x) < 2 && Math.abs(dog.getY() - y) < 2
+						&& (dog.getX() == x || dog.getY() == y)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 深さ優先で探索します。
+	 * @param x 始点のx座標
+	 * @param y 始点のy座標
+	 * @param distance 各地点への最短距離
+	 * @param path 各地点への最短経路
+	 * @param dogs 敵の犬の座標
+	 * @param map マップ
+	 */
+	private void search(final int x, final int y, final int[][] distance, final Direction[][] path,
+			final boolean[][] dogs, final Map map) {
+		final int d = distance[y][x] + 1;
+		for (final Direction direction : Direction.values()) {
+			if (direction != Direction.NONE) {
+				final int dx = direction.dx;
+				final int dy = direction.dy;
+				if (map.isAvailable(x + dx, y + dy) && !map.isWall(x + dx, y + dy)
+						&& !dogs[y + dy][x + dx] && d < distance[y + dy][x + dx]) {
+					distance[y + dy][x + dx] = d;
+					path[y + dy][x + dx] = direction;
+					this.search(x + dx, y + dy, distance, path, dogs, map);
+				}
 			}
 		}
 	}
