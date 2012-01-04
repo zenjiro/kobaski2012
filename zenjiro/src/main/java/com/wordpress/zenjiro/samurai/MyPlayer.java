39cde0df9dbcde649edce9c2a42ab6a84aebfd5a
zenjiro <zenjiro0123@gmail.com>
2012-01-04 20:40:31 +0900

    ロギング部分を削除

--- zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
+++ zenjiro/src/main/java/com/wordpress/zenjiro/samurai/MyPlayer.java
@@ -1,11 +1,6 @@
 package com.wordpress.zenjiro.samurai;
 
 import java.awt.Point;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -35,7 +30,6 @@ public class MyPlayer {
 	 */
 	public void run(final Scanner sc) {
 		this.map = Map.createOrUpdateMap(this.map, sc);
-		this.log();
 		if (this.isSamurai) {
 			final Distance distance = this.getDistance(this.map.getMySamurai(), this.map
 					.getMySamurai().getState() != CharaState.SHOGUN);
@@ -262,48 +256,9 @@ public class MyPlayer {
 	}
 
 	/**
-	 * ログ出力用のライタ
-	 */
-	private BufferedWriter writer;
-
-	/**
 	 * コンストラクタ
 	 */
 	public MyPlayer() {
 		this.isSamurai = true;
-		try {
-			final FileOutputStream fos = new FileOutputStream("log_java_jp.txt");
-			this.writer = new BufferedWriter(new OutputStreamWriter(fos));
-		} catch (final FileNotFoundException e) {
-		}
-	}
-
-	/**
-	 * ログを記録します。
-	 */
-	public void log() {
-		try {
-			this.writer.write(this.map.getRemainingTime() + ", ");
-			this.writer.write(this.map.getPlayerIndex() + ", ");
-			this.writer.write(this.map.getWidth() + ", ");
-			this.writer.write(this.map.getHeight() + "\r\n");
-			for (final Chara chara : this.map.getAllCharas()) {
-				for (int i = 0; i < 4; i++) {
-					this.writer.write(this.map.isMovable(chara, Direction.values()[i]) + ", ");
-				}
-				this.writer.write("\r\n");
-			}
-			for (final Chara samurai : this.map.getAllSamurais()) {
-				this.writer.write(this.map.isAvailable(samurai.getX(), samurai.getY()) + ", ");
-				this.writer.write("{ X = " + samurai.getX() + ", Y = " + samurai.getY() + " }"
-						+ "\r\n");
-			}
-			for (final Chara dog : this.map.getAllDogs()) {
-				this.writer.write(this.map.isAvailable(dog.getX(), dog.getY()) + ", ");
-				this.writer.write("{ X = " + dog.getX() + ", Y = " + dog.getY() + " }" + "\r\n");
-			}
-			this.writer.flush();
-		} catch (final IOException e) {
-		}
 	}
 }
