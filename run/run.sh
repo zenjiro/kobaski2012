61d7db439e11aad465bc60258f4d655d0cd3fd40
chaemon <toshihiroshimizu1986@gmail.com>
2012-01-16 21:24:48 +0900

    run.sh

--- run/run.sh
+++ run/run.sh
@@ -1,2 +1,5 @@
-java -jar SamuraiCoding2011-2.0.6.jar  "../chaemon/ai.out" "../chaemon/ai.out" "../chaemon/ai.out" "java -cp ../zenjiro/target/classes com.wordpress.zenjiro.samurai.Main"
+#java -jar SamuraiCoding2011-2.0.6.jar -q -i 0 "../chaemon/ai.out"
+java -jar SamuraiCoding2011-2.0.6.jar -i 0 -t 100000 "../chaemon/ai.out" "../chaemon/ai.out" "../chaemon/ai.out" "../chaemon/ai.out"
+#java -jar SamuraiCoding2011-2.0.6.jar -i 0 -t 2000 "../chaemon/ai.out" "../chaemon/ai.out" "../chaemon/ai.out" "java -cp ../zenjiro/target/classes com.wordpress.zenjiro.samurai.Main"
+
 #java -jar SamuraiCoding2011-2.0.6.jar  "./chaemon/SampleAI_Cpp2/ai.out" "./chaemon/SampleAI_Cpp2/ai.out" "java -cp ./zenjiro/target/classes com.wordpress.zenjiro.samurai.Main" "java -cp ./zenjiro/target/classes com.wordpress.zenjiro.samurai.Main"
